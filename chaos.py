4c6de7bab3b79071854322c6882553ad771dd6ec
AllEyesOnMyPon <klemens.janicki@gmail.com>
2025-03-31 11:38:47 +0200

    Added matplotlib installation

--- chaos.py
+++ chaos.py
@@ -1,21 +1,17 @@
-# xn+1​\=r⋅xn​⋅(1−xn​) 
+import matplotlib.pyplot as plt # Import the matplotlib library for plotting
+# xn+1​\=r⋅xn​⋅(1−xn​)  
+# This is a simple implementation of the logistic map in Python.
+r = float(input("r = ")) # r is the growth rate parameter for the logistic map
+n = int(input("n = ")) # n is the number of iterations to perform
+x_value_list = [(0, 0)] * (n + 1)   # Initialize a list to store the x values, with the first element as (0, 0) # The list will store tuples of (iteration, x value)
+x_value_list[0] = (0, float(input("x0 = "))) # x0 is the initial value (0 < x0 < 1)
 
-x0 = float(input("x0 = ")) # Initial value
-r = float(input("r = ")) # Growth rate
-# The logistic map is a polynomial mapping of degree 2. It is used to model population growth.
-n = int(input("n = ")) # Number of iterations
-# The number of iterations is the number of times the equation will be applied to the initial value.
+for counter in range(1, n + 1): # Iterate from 1 to n
+    xn = r * x_value_list[counter - 1][1] * (1 - x_value_list[counter - 1][1]) # Calculate the next x value using the logistic map formula
+    x_value_list[counter] = (counter, xn) # Store the iteration number and the x value in the list
 
-x_value_list = [x0] # List to store the values of x
+print(x_value_list)
 
-counter = 0 # Counter to keep track of the number of iterations
-
-while counter < n: # While the counter is less than the number of iterations
-    counter += 1 
-    next_value = r * x_value_list[counter - 1] * (1 - x_value_list[counter - 1]) # The logistic map equation
-    x_value_list.append(next_value) 
-
-print(x_value_list) 
 
 
 
